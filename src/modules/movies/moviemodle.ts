843444dc36451cb1d68ca51d7497658c3ff32e65
Emon Ahmed <imonshomon@gmail.com>
2025-10-12 21:21:14 +0600

    done review Schema

---
+++ src/modules/movies/moviemodle.ts
@@ -0,0 +1,8 @@
+import { Schema } from 'mongoose';
+import { TReview } from './movieInterface';
+
+const reviewSchema = new Schema<TReview>({
+  email: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+});
