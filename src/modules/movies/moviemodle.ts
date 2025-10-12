8b11859dfe7e44ea05d2430dadcb3454d6870886
Emon Ahmed <imonshomon@gmail.com>
2025-10-12 21:29:54 +0600

    done moive schema

--- src/modules/movies/moviemodle.ts
+++ src/modules/movies/moviemodle.ts
@@ -1,8 +1,21 @@
-import { Schema } from 'mongoose';
-import { TReview } from './movieInterface';
+import { Schema, model } from 'mongoose';
+import { TMovie, TReview } from './movieInterface';
 
 const reviewSchema = new Schema<TReview>({
   email: { type: String, required: true },
   rating: { type: Number, required: true },
   comment: { type: String, required: true },
 });
+
+const movieSchema = new Schema<TMovie>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  releaseDate: { type: Date, required: true },
+  review: [reviewSchema],
+  genre: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  viewCount: { type: Number, default: 0 },
+  isDeleted: { type: Boolean, default: false },
+});
+
+export const Movie = model<TMovie>('Movie', movieSchema);
